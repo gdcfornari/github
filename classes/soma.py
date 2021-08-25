78e31d97d4fc33614134f2ae49bc8db4ac2bce45
Gabriela <fornari2005@gmail.com>
2021-08-25 15:08:21 -0300

    sim sim

---
+++ classes/soma.py
@@ -0,0 +1,12 @@
+class  Soma :
+
+    @ staticmethod
+    def  calcula(array ):
+        resultado  =  0
+        for numero in  matriz :
+            resultado  =  resultado  +  numero
+            return resultado
+
+matriz = [5, 4, 3]
+resultado = Soma. calcula(matriz)
+print(resultado)
